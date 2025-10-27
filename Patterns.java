3b75213ca1626e456b225a743d249e660c164a4b
PratikChavan2005 <pratikchavan963744@gmail.com>
2025-10-27 11:40:14 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,10 @@
+
+class Patterns{
+    public static void main(String[] Args){
+        for(int i=0;i<4;i++){
+            for(int j=0;j=i;j++){
+                System.out.print("*");
+            }
+        }
+    }
+}
