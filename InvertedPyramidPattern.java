535ac5246d203cf0b8b7e2bd611cdf8cb822ac71
PratikChavan2005 <pratikchavan963744@gmail.com>
2025-11-21 23:57:09 +0530

    Add files via upload

---
+++ InvertedPyramidPattern.java
@@ -0,0 +1,22 @@
+public class InvertedPyramidPattern {
+    public static void main(String[] args) {
+        int rows = 5; 
+
+       
+        for (int i = rows; i >= 1; --i) {
+
+            
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print(" ");
+            }
+
+            
+            for (int k = 1; k <= 2 * i - 1; ++k) {
+                System.out.print("*");
+            }
+
+            
+            System.out.println();
+        }
+    }
+}
