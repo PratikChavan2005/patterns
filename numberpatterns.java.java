e186d1863bbb04bd28f6da78a0652db02cfde008
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-01-10 22:35:44 +0530

    Add files via upload

---
+++ numberpatterns.java.java
@@ -0,0 +1,39 @@
+
+// Zero-One triangle
+import java.util.*;
+
+public class numberpatterns {
+    
+   
+    public static void printPattern(int n)
+    {
+        int i, j;
+        
+        
+        for (i = 1; i <= n; i++) {
+            
+           
+            for (j = 1; j <= i; j++) {
+                
+                //
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                }
+                
+                else {
+                    System.out.print(0 + " ");
+                }
+            }
+
+            //p
+            System.out.println();
+        }
+    }
+
+    //
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
