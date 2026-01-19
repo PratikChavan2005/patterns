d078c40d801c81766f6ca23bf5502b02b874c5f8
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-01-19 21:12:55 +0530

    Add files via upload

---
+++ AlphabetPattern.java
@@ -0,0 +1,18 @@
+class AlphabetPattern{
+public static void p14(){
+
+                for(char  i='A';i<='E';i++){
+                    for(char j = 'A';j<=i;j++ ){
+                        System.out.print(j);
+                    }
+                    System.out.println();
+                }
+                 
+
+        }
+    
+    public static void main(String[] args){
+         
+            p14();
+    }
+}
