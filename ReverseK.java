617e85cdcc8d8573b3dfc500b48ef7ac22e0ddbc
PratikChavan2005 <pratikchavan963744@gmail.com>
2026-02-10 22:28:27 +0530

    Add files via upload

---
+++ ReverseK.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+ 
+public class ReverseK
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+ 
+        //Taking rows value from the user
+ 
+        System.out.println("How many rows you want in this pattern?");
+ 
+        int rows = sc.nextInt();
+ 
+        System.out.println("Here is your pattern....!!!"); 
+ 
+        for (int i = rows; i >= 1; i--)
+        {
+            //Printing spaces at the beginning of each row
+ 
+            for (int j = 2*rows-i; j > i; j--)
+            {
+                System.out.print(" ");
+            }
+ 
+            //Printing i to 1 value at the end of each row
+ 
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(j+" ");
+            }
+ 
+            System.out.println();
+        }
+ 
+        //Closing the resources
+ 
+        sc.close();
+    }
+}
